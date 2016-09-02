758f122b8b2f085df6487f9b0fd0bfded37fd017
Antonio Carlos <falcaopetri@gmail.com>
2016-09-02 11:38:39 -0300

    Minor update. Cross-PC ServerName.
    Alterei o ServerName para "(local)". Dessa forma não é necessário
    especificar o nome do computador por extenso. Basta criar a instância
    com o nome SQLEXPRESS, que é o padrão da instalação.

--- connectToSQLServer.php
+++ connectToSQLServer.php
@@ -1,5 +1,5 @@
 <?php
-$serverName = "DESKTOP-B63RV2P\sqlexpress"; //serverName\instanceName
+$serverName = "(local)\sqlexpress"; //serverName\instanceName
 
 // Since UID and PWD are not specified in the $connectionInfo array,
 // The connection will be attempted using Windows Authentication.
@@ -8,7 +8,7 @@ $conn = sqlsrv_connect( $serverName, $connectionInfo);
 
 if( $conn == false ) {
   echo "Connection could not be established.<br />";
-  die( print_r( sqlsrv_errors(), true));      
+  die( print_r( sqlsrv_errors(), true));
 }
 
 ?>
