8dc561d8d61a0d5e69d7605ae4edc8c5eada38d1
Antonio Carlos <falcaopetri@gmail.com>
2016-09-03 16:15:31 -0300

    Minor update. Conexão com o DB em UTF-8.
    O read/write de strings no banco de dados estava com problemas na
    acentuação. Adicionei um parâmetro de CharSet na configuração da conexão
    com o banco que resolve isso.

--- connectToSQLServer.php
+++ connectToSQLServer.php
@@ -3,7 +3,7 @@ $serverName = "(local)\sqlexpress"; //serverName\instanceName
 
 // Since UID and PWD are not specified in the $connectionInfo array,
 // The connection will be attempted using Windows Authentication.
-$connectionInfo = array( "Database"=>"LABBD");
+$connectionInfo = array( "Database"=>"LABBD", "CharacterSet" => "UTF-8");
 $conn = sqlsrv_connect( $serverName, $connectionInfo);
 
 if( $conn == false ) {
