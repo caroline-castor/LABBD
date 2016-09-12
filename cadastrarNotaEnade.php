63425cbe47c60cd1542617d76a151d1022c43426
huuski <uski.marco@gmail.com>
2016-09-12 12:29:03 -0300

    Add files via upload
    Update dos arquivos relacionados a ENADE.
    Cadastro e verificação de desempenho por curso/individual.

---
+++ cadastrarNotaEnade.php
@@ -0,0 +1,29 @@
+<?php require_once 'connectToSQLServer.php';
+
+
+// Armazena a query de insercao
+$tsql = "INSERT INTO ENADE_realizado(ano,cpf_estudante,data,nota,curso) VALUES (?,?,?,?,?)";
+
+$parameters = [$_POST['ano'],$_POST['cpf'],$_POST['data'],$_POST['nota'],$_POST['curso']];
+
+//executa a query tsql, resultado fica em stmt
+$stmt = sqlsrv_query($conn,$tsql,$parameters);
+//verifica se o retorno é falso
+if( $stmt === false ){
+echo "Statement could not be executed.\n";
+ die( print_r( sqlsrv_errors(), true));
+} 
+
+
+//se nenhum erro ocorrer, redireciona a pagina
+echo '<meta HTTP-EQUIV="Refresh" CONTENT="0.1; URL=visualizarEnade.php">';
+
+/* Free statement and connection resources. */
+sqlsrv_free_stmt( $stmt);
+
+sqlsrv_close( $conn);
+
+
+
+
+?>
